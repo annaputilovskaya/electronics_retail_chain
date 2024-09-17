b9abef35acaad549d927ea289c8deeec12f969d7
putilovskaya <putilovskayaa@mail.ru>
2024-09-18 00:04:11 +0300

    write tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,122 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from products.models import Product
+from suppliers.models import Organization, Contacts
+from users.models import User
+
+
+class ProductTestCase(APITestCase):
+    """
+    Тестирование модели продукта.
+    """
+    def setUp(self):
+        """
+        Создание пользователя, организации, контактов и продуктов
+        для тестирования.
+        """
+
+        self.user1 = User.objects.create(email="user1@example.com")
+        self.organization = Organization.objects.create(
+            name='Test Organization',
+            organization_type='individual',
+            )
+        self.contacts = Contacts.objects.create(
+            country='Test Country',
+            city='Test City',
+            street='Test Street',
+            house='123',
+            organization=self.organization,
+        )
+        self.product = Product.objects.create(
+            name="Test Product",
+            model=100,
+            launched_at='2022-01-01',
+            organization=self.organization
+        )
+        self.product2 = Product.objects.create(
+            name="Test Product",
+            model=100,
+            launched_at='2022-01-01',
+            organization=self.organization
+        )
+        self.client.force_authenticate(user=self.user1)
+
+    def test_str_product(self):
+        """
+        Тестирование строчного представления продукта.
+        """
+        product = Product.objects.get(pk=self.product.pk)
+        self.assertEqual(str(product), f"{self.product.name} ({self.product.model})")
+
+    def test_product_create(self):
+        """
+        Тестирование создания нового продукта.
+        """
+        url = reverse("products:products-list")
+        data = [
+            {
+                "name": "Test Product 2",
+                "model": 200,
+                "launched_at": "2022-02-01",
+                "organization": self.organization.pk,
+                },
+            {
+                "name": "Test Product 3",
+                "model": 200,
+                "launched_at": "2122-02-01",
+                "organization": self.organization.pk,
+                },
+            ]
+        # Тестируем корректное создание продукта
+        response = self.client.post(url, data=data[0])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Product.objects.get(name="Test Product 2"))
+
+        # Тестируем создание продукта с некорректной датой
+        response = self.client.post(url, data=data[1])
+        message = response.json()["non_field_errors"]
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            message, ["Дата выхода продукта на рынок не может быть позднее текущей даты."]
+        )
+
+    def test_product_update(self):
+        """
+        Тестирование изменения существующего продукта.
+        """
+        url = reverse("products:products-detail", args=(self.product.pk,))
+        data = {
+            "name": "Updated Test Product",
+        }
+        response = self.client.patch(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.get(pk=self.product.pk).name, "Updated Test Product")
+
+    def test_product_delete(self):
+        """
+        Тестирование удаления существующего продукта.
+        """
+        url = reverse("products:products-detail", args=(self.product.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Product.objects.filter(pk=self.product.pk).exists())
+
+    def test_product_retrieve(self):
+        """
+        Тестирование получения продукта по ID.
+        """
+        url = reverse("products:products-detail", args=(self.product.pk,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()["name"], "Test Product")
+
+    def test_product_list(self):
+        """
+        Тестирование получения списка всех продуктов.
+        """
+        url = reverse("products:products-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()["results"]), 2)
--- suppliers/tests.py
+++ suppliers/tests.py
@@ -1,3 +1,97 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from suppliers.models import Organization, Contacts
+from users.models import User
+
+
+class OrganizationTestCase(APITestCase):
+    """
+    Тестирование модели организации.
+    """
+    def setUp(self):
+        """
+        Создание пользователя, организации и контактов
+        для тестирования.
+        """
+
+        self.user1 = User.objects.create(email="user1@example.com")
+        self.organization1 = Organization.objects.create(
+            name='Test Organization 1',
+            organization_type='factory',
+            )
+        self.organization2 = Organization.objects.create(
+            name='Test Organization 2',
+            organization_type='retail',
+            supplier=self.organization1,
+            )
+        self.contacts1 = Contacts.objects.create(
+            country='Test Country',
+            city='Test City',
+            street='Test Street',
+            house='123',
+            organization=self.organization1,
+        )
+        self.contacts2 = Contacts.objects.create(
+            country='Test Country',
+            city='Test City',
+            street='Test Street',
+            house='123',
+            organization=self.organization2,
+        )
+
+        self.client.force_authenticate(user=self.user1)
+
+    def test_str_organization(self):
+        """
+        Тестирование строчного представления организации.
+        """
+        organization = Organization.objects.get(pk=self.organization1.pk)
+        self.assertEqual(str(organization), self.organization1.name)
+
+    def test_organization_retrieve(self):
+        """
+        Тестирование получения информации об организации по ID.
+        """
+        url = reverse("suppliers:organization-detail", args=[self.organization1.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], self.organization1.name)
+
+    def test_organization_list(self):
+        """
+        Тестирование получения списка всех организаций.
+        """
+        url = reverse("suppliers:organization-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual((response.data["count"]), Organization.objects.count())
+
+    def test_organization_delete(self):
+        """
+        Тестирование удаления организации по ID.
+        """
+        url = reverse("suppliers:organization-detail", args=[self.organization1.pk])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Organization.objects.all().count(), 1)
+
+    def test_organization_update(self):
+        """
+        Тестирование изменения информации об организации.
+        """
+        url = reverse("suppliers:organization-detail", args=[self.organization1.pk])
+        data = [
+            {"name": "Organ"},
+            {"debt": 100},
+            ]
+
+        # Тестируем корректное изменениие информации об организации.
+        response = self.client.patch(url, data[0])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], data[0]["name"])
+
+        # Тестируем изменение долга организации.
+        response = self.client.patch(url, data[1])
+        self.assertNotEqual(response.data["debt"], data[1]["debt"])
